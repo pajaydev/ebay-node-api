f85a11182c61e65501663360b0f0acb51cd92c86
ajay2507 <pajay2507@gmail.com>
2018-05-24 20:41:06 -0700

    fixed search items bug

--- src/buy-api.js
+++ src/buy-api.js
@@ -5,16 +5,23 @@ const getItem = function (itemId) {
     if (!this.options.access_token) throw new Error("Missing Access token, Generate access token");
     const auth = "Bearer " + this.options.access_token;
     const id = encodeURIComponent(itemId);
-    return makeRequest('api.ebay.com', `/buy/browse/v1/item/${id}`, 'GET', this.options.body, auth).then((result) => {
-        console.log("Success");
-        let resultJSON = JSON.parse(result);
-        //this.setAccessToken(resultJSON);
-        return resultJSON;
-    });
+    // return makeRequest('api.ebay.com', `/buy/browse/v1/item/${id}`, 'GET', this.options.body, auth).then((result) => {
+    //     console.log("Success");
+    //     let resultJSON = JSON.parse(result);
+    //     //this.setAccessToken(resultJSON);
+    //     return resultJSON;
+    // });
+
+    return getRequest("https://api.ebay.com/buy/browse/v1/item_summary/search?q=drone&limit=3").then((data) => {
+        let result = JSON.parse(data);
+        console.log(result);
+        return result;
+    }, (error) => {
+        console.log(error);
+    })
 };
 
 const getItemByLegacyId = function (legacyOptions) {
-    console.log(legacyOptions);
     if (!legacyOptions) throw new Error("Error Required input to get Items By LegacyID");
     if (!this.options.access_token) throw new Error("Missing Access token, Generate access token");
     if (!legacyOptions.legacyItemId) throw new Error("Error Legacy Item Id is required");
@@ -39,28 +46,27 @@ const getItemByItemGroup = function (itemGroupId) {
     const auth = "Bearer " + this.options.access_token;
     return new Promise((resolve, reject) => {
         makeRequest('api.ebay.com', `/buy/browse/v1/item/get_items_by_item_group?item_group_id=${itemGroupId}`, 'GET', this.options.body, auth).then((result) => {
-            resolve(JSON.parse(result));
+            resolve(result);
         });
     })
 };
 
 const searchItems = function (searchConfig) {
-    console.log(searchConfig.keyword);
     if (!searchConfig) throw new Error("Error --> Missing or invalid input parameter to search");
     if (!searchConfig.keyword && !searchConfig.categoryId) throw new Error("Error --> Keyword or category id is required in query param");
     if (!this.options.access_token) throw new Error("Error -->Missing Access token, Generate access token");
-    if (searchConfig.fieldgroups.length > 0 && !Array.isArray(searchConfig.fieldgroups)) throw new Error("Error -->Field groups should be an array");
     const auth = "Bearer " + this.options.access_token;
-    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword + "&" : "";
-    queryParam = queryParam + searchConfig.categoryId ? "category_ids=" + searchConfig.categoryId + "&" : '';
-    queryParam = queryParam + searchConfig.limit ? "limit=" + searchConfig.limit + "&" : "";
-    queryParam = queryParam + searchConfig.fieldgroups.length > 0 ? "fieldgroups=" + searchConfig.fieldgroups.toString() + "&" : "";
-    queryParam = queryParam + searchConfig.filter != undefined ? "filter=" + JSON.stringify(searchConfig.filter).replace(/[{}]/g, "") : "";
+    let queryParam = searchConfig.keyword ? "q=" + searchConfig.keyword : "";
+    queryParam = queryParam + (searchConfig.categoryId ? "&category_ids=" + searchConfig.categoryId : '');
+    queryParam = queryParam + (searchConfig.limit ? "&limit=" + searchConfig.limit : "");
+    if (searchConfig.fieldgroups != undefined) queryParam = queryParam + "&fieldgroups=" + searchConfig.fieldgroups.toString();
+    if (searchConfig.filter != undefined) queryParam = queryParam + "&filter=" + JSON.stringify(searchConfig.filter).replace(/[{}]/g, "");
+    console.log(queryParam);
     return new Promise((resolve, reject) => {
-        makeRequest('api.ebay.com', `buy/browse/v1/item_summary/search?${queryparam}`, 'GET', this.options.body, auth).then((result) => {
-            resolve(JSON.parse(result));
+        makeRequest('api.ebay.com', `/buy/browse/v1/item_summary/search?${queryParam}`, 'GET', this.options.body, auth).then((result) => {
+            resolve(result);
         });
-    })
+    });
 };
 
 module.exports = {
