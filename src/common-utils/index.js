7245e12fb38c265bcd75345e0763098a8947532d
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-05-28 15:32:39 +0700

    Remove oauth functions from utils

--- src/common-utils/index.js
+++ src/common-utils/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const { makeRequest } = require('../request');
 
-const base64Encode = (encodeData) => {
+const base64Encode = encodeData => {
     const buff = Buffer.from(encodeData);;
     return buff.toString('base64');
 };
@@ -42,23 +42,6 @@ const constructAdditionalParams = (options) => {
 };
 
 module.exports = {
-    setAccessToken: function (token) {
-        this.options.access_token = token;
-    },
-    getAccessToken: function () {
-        if (!this.options.clientID) throw new Error('Missing Client ID');
-        if (!this.options.clientSecret) throw new Error('Missing Client Secret or Cert Id');
-        if (!this.options.body) throw new Error('Missing Body, required Grant type');
-        const encodedStr = base64Encode(this.options.clientID + ':' + this.options.clientSecret);
-        const self = this;
-        const auth = 'Basic ' + encodedStr;
-        this.options.contentType = 'application/x-www-form-urlencoded';
-        return makeRequest(this.options, '/identity/v1/oauth2/token', 'POST', auth).then((result) => {
-            const resultJSON = JSON.parse(result);
-            self.setAccessToken(resultJSON.access_token);
-            return resultJSON;
-        });
-    },
     setSiteId: function (siteId) {
         this.options.siteId = siteId;
     },
@@ -70,11 +53,9 @@ module.exports = {
         return data.toUpperCase();
     },
 
-    // Returns if a value is a string
-    isString(value) {
-        return typeof value === 'string' || value instanceof String;
+    isString(value) { 
+        return typeof value === 'string' || value instanceof String; 
     },
-
     // Returns if object is empty or not
     isEmptyObj(obj) {
         for (let key in obj) {
