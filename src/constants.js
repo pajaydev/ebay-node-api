52e00c44709f376cb7122733811508aa03e36981
ajay2507 <pajay2507@gmail.com>
2020-08-09 16:54:47 -0700

    adding deals api

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@
 module.exports = {
     PROD_OAUTHENVIRONMENT_WEBENDPOINT: 'https://auth.ebay.com/oauth2/authorize',
     SANDBOX_OAUTHENVIRONMENT_WEBENDPOINT: 'https://auth.sandbox.ebay.com/oauth2/authorize',
+    DEALS_BASE_URL: 'http://www.ebay.com/rps/feed/v1.1',
     PROD_BASE_URL: 'api.ebay.com',
     SANDBOX_BASE_URL: 'api.sandbox.ebay.com',
     BASE_SVC_URL: 'svcs.ebay.com',
