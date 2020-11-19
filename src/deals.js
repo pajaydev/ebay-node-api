c45e8f0e7db94aa72b524bb003ee90c6490d7257
ajay2507 <pajay2507@gmail.com>
2020-11-18 23:59:02 -0800

    fix lint

--- src/deals.js
+++ src/deals.js
@@ -1,18 +1,19 @@
-"use strict";
-
-const axios = require("axios");
-const { parseObj } = require("./common-utils/index");
-const { DEALS_BASE_URL } = require("./constants");
+'use strict';
+/* eslint-disable indent */
+const axios = require('axios');
+const { parseObj } = require('./common-utils/index');
+const { DEALS_BASE_URL } = require('./constants');
 
 module.exports = {
+
   /**
    * @deprecated will be removed in a future major version.
    * @method getDeals {Function}
    * get deals based on site, category across ebay like (Today's top ten deals)
    * @param {Object} options (optional)
    */
-  getDeals: async function(options) {
-    if (!this.options.clientID) throw new Error("Missing App id or client id");
+  getDeals: async function (options) {
+    if (!this.options.clientID) throw new Error('Missing App id or client id');
     if (!options) options = {};
     const countryCode = options.countryCode
       ? options.countryCode
