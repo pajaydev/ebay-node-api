6d1604145e692b6b7bc71738472c25e3fc849b69
David <davidgalet@gmail.com>
2021-03-28 15:41:38 +0200

    Create account-api.js

---
+++ src/sale-api/account-api.js
@@ -0,0 +1,18 @@
+'use strict';
+const { callbackRequest } = require('./callbackRequest');
+
+/**
+ * Url
+ * @type {String}
+ */
+const URL_EBAY = '/sell/account/v1/fulfillment_policy';
+
+/**
+ * String of url for scope oauth
+ * @type {String}
+ */
+const SCOPE_API = 'https://api.ebay.com/oauth/api_scope/sell.account';
+
+
+module.exports = {
+};
